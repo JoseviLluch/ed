7b290ac5ef3ca55a3ee183c66c3f366b310f8c79
JoseviLluch <dam.jlluch@institutoserpis.org>
2015-10-05 11:57:13 +0200

    Añadido JHolaMundo

---
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package org.institutoserpis.ed.HolaMundo;
+
+public class HolaMundo {
+	
+	public static void main (String[] args){
+		System.out.println("Hola mundo desde eclipse");
+		
+		
+		
+	}
+
+}
