e279c8e88137c7e5684204d2ba5ea0de2746505b
JoseviLluch <dam.jlluch@institutoserpis.org>
2015-10-21 08:51:38 +0200

    JArray: resuelto suma; test menor.

---
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -0,0 +1,18 @@
+package org.institutoserpis.ed;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+	
+	public static void main (String[] args){
+		int nombre;
+		Scanner scan = new Scanner(System.in);
+		nombre = scan.nextInt();
+		
+		System.out.printf("Hola %s desde eclipse");
+		
+		
+		
+	}
+
+}
